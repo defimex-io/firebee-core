75174d004c07ca55755305d9cbe5f453704754fb
lovemjj <516370418@qq.com>
2020-10-13 10:00:07 +0800

    Update firebee.ts
    update level type to i32; instantiate UserDB

--- firebee.ts
+++ firebee.ts
@@ -1,9 +1,9 @@
-import { Address, DB, Store } from "keystore_wdc/lib";
+import { Address, DB, Store } from "./node_modules/keystore_wdc/lib";
 import { User, MAX_LEVEL , ZERO_ADDRESS, X3, X6} from "./types";
-import {___idof, ABI_DATA_TYPE, Context, Globals, U256} from "keystore_wdc/lib/index";
+import {___idof, ABI_DATA_TYPE, Context, Globals, U256} from "./node_modules/keystore_wdc/lib/index";
 
 class UserDB{
-    static USER_DB = Store.from<Address, ArrayBuffer>('user')
+    static USER_DB : Store<Address, ArrayBuffer> = Store.from<Address, ArrayBuffer>('user');
 
     hasUser(addr: Address): bool{
         return UserDB.USER_DB.has(addr);
@@ -21,10 +21,11 @@ class UserDB{
     }
 }
 
-const idToAddress = Store.from<u64, Address>('idToAddress');
-const userIds = Store.from<u64, Address>('userIds');
-const levelPrice = Store.from<u64, u64>('levelPrice');
+const idToAddress = Store.from<Address, U256>('idToAddress');
+const userIds = Store.from<Address, U256>('userIds');
+const levelPrice = Store.from<i32, U256>('levelPrice');
 const firstPrice = 10;
+const userDB = new UserDB();
 
 export function init(ownerAddress: Address, lastUserId: U256): Address {
     //当前最新的可用ID，由于合约部署时，创始人会使用1作为ID，因此从2开始作为当前最新可用ID
@@ -34,8 +35,8 @@ export function init(ownerAddress: Address, lastUserId: U256): Address {
     levelPrice.set(1, firstPrice);
 
     //每个等级的激活价格都是前一个等级的两倍
-    for (var i = 2; i <= MAX_LEVEL; i++) {
-        let nextLeverPrice = levelPrice.get(i-1) * 2;
+    for (let i = 2; i <= MAX_LEVEL; i++) {
+        let nextLeverPrice = levelPrice.getOrDefault(i-1, U256.ZERO) * 2;
         levelPrice.set(i, nextLeverPrice);
     }
 
@@ -47,19 +48,19 @@ export function init(ownerAddress: Address, lastUserId: U256): Address {
     let user = new User(1,ZERO_ADDRESS,0);
 
     //创始人的所有级别矩阵，默认全部激活，不用付费
-    for (var i = 1; i <= MAX_LEVEL; i++) {
+    for (let i = 1; i <= MAX_LEVEL; i++) {
         user.activeX3Levels[i] = true;
         user.activeX6Levels[i] = true;
     }
 
     //将创始人加入到用户记录数组中
-    UserDB.setUser(ownerAddress, user);
+    userDB.setUser(ownerAddress, user);
 
     //将创始人记录到ID记录数组中
-    idToAddress.set(1, ownerAddress);
+    idToAddress.set(ownerAddress, i32(1));
 
     //用户ID为1的记录为创始人
-    userIds.set(1, ownerAddress);
+    userIds.set(ownerAddress, i32(1));
 
     return Context.self();
 }
@@ -74,11 +75,11 @@ export function resetOwner(ownerAddress: Address): void {
     const msg = Context.msg();
     let owner = Globals.get<Address>('owner');
     assert(msg.sender == owner, 'sender is not owner');
-    let reUser = UserDB.getUser(owner);
-    UserDB.removeUser(owner);
-    UserDB.setUser(ownerAddress, reUser);
-    idToAddress.set(1, ownerAddress);
-    userIds.set(1, ownerAddress);
+    let reUser = userDB.getUser(owner);
+    userDB.removeUser(owner);
+    userDB.setUser(ownerAddress, reUser);
+    idToAddress.set(ownerAddress, i32(1));
+    userIds.set(ownerAddress, i32(1));
     Globals.set<Address>('owner', ownerAddress);
     return;
 }
@@ -87,20 +88,20 @@ export function resetOwner(ownerAddress: Address): void {
 //参数为：推荐人地址
 //注意，这个方法是一个可以外部调用的方法，同时允许转账ETH,因此带有payable修饰符
 //内部调用了真正处理新用户注册的registration方法
-export function registrationExt(referrerAddress : Address) {
+export function registrationExt(referrerAddress : Address): void{
     const msg = Context.msg();
     registration(msg.sender, referrerAddress, msg.amount);
 }
 
 //购买级别
-export function buyNewLevel(matrix : u64, level : u64) {
+export function buyNewLevel(matrix : u64, level : i32): void {
     const msg = Context.msg();
-    assert(UserDB.hasUser(msg.sender), "user is not exists. Register first.");
+    assert(userDB.hasUser(msg.sender), "user is not exists. Register first.");
     assert(matrix == 1 || matrix == 2, "invalid matrix");
-    assert(msg.amount == levelPrice[level], "invalid price");
+    assert(msg.amount == levelPrice.getOrDefault(level, U256.ZERO), "invalid price");
     assert(level > 1 && level <= MAX_LEVEL, "invalid level");
 
-    let sendUser = UserDB.getUser(msg.sender);
+    let sendUser = userDB.getUser(msg.sender);
 
     if (matrix == 1) {
         assert(!sendUser.activeX3Levels[level], "level already activated");
@@ -112,7 +113,7 @@ export function buyNewLevel(matrix : u64, level : u64) {
         let freeX3Referrer = findFreeX3Referrer(msg.sender, level);
         sendUser.x3Matrix[level].currentReferrer = freeX3Referrer;
         sendUser.activeX3Levels[level] = true;
-        UserDB.setUser(msg.sender, sendUser);
+        userDB.setUser(msg.sender, sendUser);
         updateX3Referrer(msg.sender, freeX3Referrer, level);
 
         Context.emit<Upgrade>(new Upgrade(msg.sender, freeX3Referrer, 1, level));
@@ -136,7 +137,7 @@ export function buyNewLevel(matrix : u64, level : u64) {
 //新用户注册方法
 //参数为：新用户地址、推荐人地址
 //注意，所谓的新用户注册，就是给某个已经加入到矩阵的地址（推荐人地址）投资，同时要符合级别的要求，新用户只能从第一级开始
-function registration(userAddress : Address,referrerAddress : Address, amount: U256) {
+function registration(userAddress : Address,referrerAddress : Address, amount: U256) : void {
 
     /*这里是一些前置条件的校验，条件分别为：
       1、转账的ETH必须是0.5个，为什么呢？因为必须同时激活X3与X6的第一个级别，分别是0.025，加起来就得是0.5,
@@ -145,8 +146,8 @@ function registration(userAddress : Address,referrerAddress : Address, amount: U
       3、推荐人必须是已经在矩阵中存在的
     */
     assert(amount == firstPrice, "registration cost ${ firstPrice }");
-    assert(!UserDB.hasUser(userAddress), "user exists");
-    assert(UserDB.hasUser(referrerAddress), "referrer not exists");
+    assert(!userDB.hasUser(userAddress), "user exists");
+    assert(userDB.hasUser(referrerAddress), "referrer not exists");
 
     /*这里一段代码是计算用户地址的长度，之所以要计算，是因为要禁止合约账户参与
       在以太坊中，通过extcodesize计算出来的codesize，如果是合约地址，事务携带的payload就不会是0
@@ -169,16 +170,16 @@ function registration(userAddress : Address,referrerAddress : Address, amount: U
     //新用户的推荐人地址
     user.referrer = referrerAddress;
     //用户ID->用户地址
-    idToAddress.set(lastUserId, userAddress);
+    idToAddress.set(userAddress, lastUserId);
 
     //新用户记录到ID总册中，同时最新的id+1
-    userIds.set(lastUserId, userAddress);
-    Globals.set<U256>('lastUserId', lastUserId++);
+    userIds.set(userAddress, lastUserId);
+    Globals.set<U256>('lastUserId', lastUserId+1);
 
-    let referrerUser = UserDB.getUser(referrerAddress);
+    let referrerUser = userDB.getUser(referrerAddress);
     //用户推荐人地址的团队总数+1
     referrerUser.partnersCount = referrerUser.partnersCount + 1;
-    UserDB.setUser(referrerAddress, referrerUser);
+    userDB.setUser(referrerAddress, referrerUser);
 
     /*确认X3的推荐人地址
     这里要注意，不是上述的推荐人地址，而是用户所在X3矩阵的实际推荐人地址，这个地方容易产生混淆性，
@@ -193,7 +194,7 @@ function registration(userAddress : Address,referrerAddress : Address, amount: U
     user.x3Matrix[1] = userx3Matrix;
 
     //保存新用户数据
-    UserDB.setUser(userAddress, user);
+    userDB.setUser(userAddress, user);
 
     //将确认到的X3推荐人地址，填入新用户X3第一个矩阵的推荐人地址中
     //注意参数中的freeX3Referrer，这个地址如上所述，是矩阵实际推荐人
@@ -209,30 +210,30 @@ function registration(userAddress : Address,referrerAddress : Address, amount: U
 //检查用户推荐人X3模块下某个矩阵是否激活
 //参数为：传入用户地址、X3矩阵级别序列号，并获取实际推荐人地址
 //最终获得的结果是：用户地址的推荐人地址（向上遍历），如果有已经激活指定级别X3矩阵的，就返回那个推荐人地址
-function findFreeX3Referrer(userAddress : Address , level : u64): Address {
+function findFreeX3Referrer(userAddress : Address , level : i32): Address {
     //这里用一个while循环来向上遍历
     while (true) {
         //检测用户推荐人的X3矩阵是否激活，这里注意矩阵级别，也就是下面代码中的level，
         //完整的说，是检测用户推荐人的指定级别的矩阵是否激活
-        if (UserDB.getUser(UserDB.getUser(userAddress).referrer).activeX3Levels[level]) {
+        if (userDB.getUser(userDB.getUser(userAddress).referrer).activeX3Levels[level]) {
             //如果是激活的，就返回推荐人地址
-            return UserDB.getUser(userAddress).referrer;
+            return userDB.getUser(userAddress).referrer;
         }
 
         //如果用户推荐人指定级别的X3矩阵没有激活，就将userAddress更新为用户推荐人地址，继续上述的while循环判断
-        userAddress = UserDB.getUser(userAddress).referrer;
+        userAddress = userDB.getUser(userAddress).referrer;
     }
 }
 
 //更新X3矩阵
 //参数为：用户地址、推荐人地址（在调用时，会赋值为实际推荐人地址）、矩阵级别
-function updateX3Referrer(userAddress: Address,referrerAddress: Address, level: u64): void {
+function updateX3Referrer(userAddress: Address,referrerAddress: Address, level: i32): void {
 
     //将当前用户地址填入推荐人的X3矩阵下面
     //根据理解，填入的时候，可能处于3种位置，其中第3个位置会触发矩阵重置以及推荐人地址的复投
-    let referrerAddressUser = UserDB.getUser(referrerAddress);
+    let referrerAddressUser = userDB.getUser(referrerAddress);
     referrerAddressUser.x3Matrix[level].referrals.push(userAddress);
-    UserDB.setUser(referrerAddress, referrerAddressUser);
+    userDB.setUser(referrerAddress, referrerAddressUser);
 
     //如果推荐人指定级别的X3矩阵的下级点位少于3个，也就是还没都点亮
     if (referrerAddressUser.x3Matrix[level].referrals.length < 3) {
@@ -289,28 +290,28 @@ function updateX3Referrer(userAddress: Address,referrerAddress: Address, level: 
         //注意，向上遍历时，最终如果到达创始人地址，则总是能接收
         sendWDCDividends(owner, userAddress, 1, level);
         //创始人地址对应级别的团队总数+1
-        let ownerUser = UserDB.getUser(owner);
+        let ownerUser = userDB.getUser(owner);
         ownerUser.x3Matrix[level].reinvestCount++;
-        UserDB.setUser(owner, ownerUser);
+        userDB.setUser(owner, ownerUser);
 
         //发送复投事件
         Context.emit<Reinvest>(new Reinvest(owner, ZERO_ADDRESS, userAddress, 1, level));
     }
-    UserDB.setUser(referrerAddress, referrerAddressUser);
+    userDB.setUser(referrerAddress, referrerAddressUser);
 }
 
 //发送ETH
 //参数为：接收者地址、发送地址、对应X3/X6矩阵、矩阵级别
 //同时可以看到这个方法是一个私有方法，也就是不允许在外部直接调用
-function sendWDCDividends(userAddress: Address , _from: Address , matrix : u64 , level: u64 ): void {
+function sendWDCDividends(userAddress: Address , _from: Address , matrix : u64 , level: i32 ): void {
 
     /*首先要确定ETH接收人的地址
       这里使用了一个方法findEthReceiver来进行确定
       返回值包含两个值，一个是确定的接收人地址，一个表示是否奖金滑落
     */
     let findWdcReceiver = findWdcReceiver(userAddress, _from, matrix, level);
-    let receiver = findWdcReceiver.receiver;
-    let isExtraDividends = findWdcReceiver.isExtraDividends;
+    let receiver: Address = findWdcReceiver[i32(0)];
+    let isExtraDividends = findWdcReceiver[i32(1)];
     //使用send方法向receiver地址转账，
     receiver.transfer(levelPrice.get(level));
 
@@ -322,11 +323,13 @@ function sendWDCDividends(userAddress: Address , _from: Address , matrix : u64 ,
 
 //确定eth的接收人地址，寻找每一笔交易ETH真正的接收者，检查推荐人的对应矩阵是否阻塞
 //参数：接收地址、发送地址、矩阵类型、矩阵级别
-function findWdcReceiver(userAddress: Address , _from: Address , matrix: u64 , level: u64) {
+function findWdcReceiver(userAddress: Address , _from: Address , matrix: u64 , level: i32) : any[] {
     //将参数中的接收地址赋值给receiver变量
     let receiver = userAddress;
     //
     let isExtraDividends = true;
+
+    let result = [];
 
     //如果是X3矩阵
     if (matrix == 1) {
@@ -334,17 +337,19 @@ function findWdcReceiver(userAddress: Address , _from: Address , matrix: u64 , l
         //这里又是通过一个循环来进行寻找
         while (true) {
             //如果接收者地址对应矩阵的blocked状态是true
-            if (UserDB.getUser(receiver).x3Matrix[level].blocked) {
+            if (userDB.getUser(receiver).x3Matrix[level].blocked) {
                 //发送奖金烧伤事件
                 Context.emit<MissedWdcReceive>(new MissedWdcReceive(receiver, _from, 1, level));
                 //将isExtraDividends的状态更新为true，表示奖金将滑落
                 isExtraDividends = true;
                 //将接收者地址更新为当前接收者地址所在X3矩阵，对应矩阵级别的，有效推荐人地址
                 //关于currentReferrer的获得，在之前的方法中已经有过
-                receiver = UserDB.getUser(receiver).x3Matrix[level].currentReferrer;
+                receiver = userDB.getUser(receiver).x3Matrix[level].currentReferrer;
             } else {
+                result.push(receiver);
+                result.push(isExtraDividends);
                 //返回有效的接收者地址，以及滑落状态
-                return { receiver, isExtraDividends };
+                return result;
             }
         }
     } else {
@@ -353,16 +358,18 @@ function findWdcReceiver(userAddress: Address , _from: Address , matrix: u64 , l
             //同样是一个循环的检索过程
 
             //如果接收地址对应级别的X6矩阵是blocked状态，则继续向上搜索
-            if (UserDB.getUser(receiver).x6Matrix[level].blocked) {
+            if (userDB.getUser(receiver).x6Matrix[level].blocked) {
                 //发送奖金滑落事件
                 Context.emit<MissedWdcReceive>(new MissedWdcReceive(receiver, _from, 2, level));
                 //更新滑落状态为true
                 isExtraDividends = true;
                 //将接收人地址更新为currentReferrer
-                receiver = UserDB.getUser(receiver).x6Matrix[level].currentReferrer;
+                receiver = userDB.getUser(receiver).x6Matrix[level].currentReferrer;
             } else {
+                result.push(receiver);
+                result.push(isExtraDividends);
                 //如果不是blocked状态，则返回，返回值包含有效的接收人地址以及滑落状态
-                return {receiver, isExtraDividends};
+                return result;
             }
         }
     }
@@ -374,19 +381,19 @@ export function __idof(type: ABI_DATA_TYPE): u32 {
 }
 
 @unmanaged class NewUserPlace {
-    constructor(readonly user: Address, readonly referrer: Address, readonly matrix: u64, readonly level: u64, readonly place: U256) { }
+    constructor(readonly user: Address, readonly referrer: Address, readonly matrix: u64, readonly level: i32, readonly place: i32) { }
 }
 
 @unmanaged class MissedWdcReceive {
-    constructor(readonly receiver: Address, readonly from: Address, readonly matrix: u64, readonly level: u64) { }
+    constructor(readonly receiver: Address, readonly from: Address, readonly matrix: u64, readonly level: i32) { }
 }
 
 @unmanaged class SentExtraEthDividends {
-    constructor(readonly from: Address, readonly receiver: Address, readonly matrix: u64, readonly level: u64) { }
+    constructor(readonly from: Address, readonly receiver: Address, readonly matrix: u64, readonly level: i32) { }
 }
 
 @unmanaged class Reinvest {
-    constructor(readonly user: Address, readonly currentReferrer: Address, readonly caller: Address, readonly matrix: u64, readonly level: u64) { }
+    constructor(readonly user: Address, readonly currentReferrer: Address, readonly caller: Address, readonly matrix: u64, readonly level: i32) { }
 }
 
 @unmanaged class Registration {
@@ -394,5 +401,5 @@ export function __idof(type: ABI_DATA_TYPE): u32 {
 }
 
 @unmanaged class Upgrade {
-    constructor(readonly user: Address, readonly referrer: Address, readonly matrix: U256, readonly level: U256) { }
+    constructor(readonly user: Address, readonly referrer: Address, readonly matrix: u64, readonly level: i32) { }
 }
